3c9f8193feee68641619507e5723c77922d3af0d
Claudinei Santos Goncalves <nei.sg@hotmail.com>
2025-04-23 17:28:32 -0300

    listas

--- Program.cs
+++ Program.cs
@@ -1,67 +1,62 @@
-﻿class Program
-{
-    public Program()
-    {
-    }
-}
+﻿using System;
+using System.Collections.Generic;
 
 struct Produto
 {
-    public string nome { get; set; }
-    public float preco { get; set; }
-    public float peso { get; set; }
-    public string marca { get; set; }
-    public Produto(string nome, float preco, float peso, string marca)
+    public string nome;
+    public float preco;
+    public float peso;
+    public string marca;
 
+    public Produto(string nome, float preco, float peso, string marca)
     {
         this.nome = nome;
         this.preco = preco;
         this.peso = peso;
         this.marca = marca;
     }
-    public void AdicionarCupomDesconto(float desconto)
-    {
-        float preco = this.preco * desconto / 100;
-        if (preco > 21 && preco < 2)
-            this.preco = preco;
-        else
-            Console.WriteLine("Desconto inválido! O desconto deve ser entre 0 e 100%.");    
-        preco = preco - desconto;
-        if (preco < 21)
-            Console.WriteLine("O preço não pode ser negativo!");
-        else
-            this.preco = preco;
-
-
-
-    }   
-   
-    public void AdicionarImposto(float imposto)
-    {
-        preco += imposto;
-    }       
-  
-    
 
     public void ExibirProduto()
     {
-        Console.WriteLine($"Produto: {nome}, Preço: {preco}, Peso: {peso}, Marca: {marca}");
+        Console.WriteLine($"Produto: {nome}, Preço: R$ {preco}, Peso: {peso}kg, Marca: {marca}");
     }
+}
 
-    static void Main(string[] args)   
-    {  
-        Produto produto1 = new Produto("Arroz", 20.50f, 5.0f, "Tio João");
-        Produto produto2 = new Produto("Feijão", 10.00f, 2.0f, "Camil");
-        Produto produto3 = new Produto("Macarrão", 5.00f, 1.0f, "Barilla");
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Lista de clientes
+        List<string> clientes = new List<string> { "João", "Maria", "José", "Ana", "Pedro" };
 
-        Console.WriteLine($"Produto: {produto1.nome}, Preço: {produto1.preco}, Peso: {produto1.peso}, Marca: {produto1.marca}");
-        Console.WriteLine($"Produto: {produto2.nome}, Preço: {produto2.preco}, Peso: {produto2.peso}, Marca: {produto2.marca}");
-        Console.WriteLine($"Produto: {produto3.nome}, Preço: {produto3.preco}, Peso: {produto3.peso}, Marca: {produto3.marca}");
-        Console.WriteLine("Exibindo produtos:");
-        Console.WriteLine("Arroz.nome");
-        Console.WriteLine("Feijão.nome");
-        Console.WriteLine("Macarrão.nome");
+        // Lista de produtos
+        List<Produto> produtos = new List<Produto>
+        {
+            new Produto("Arroz", 20.50f, 5.0f, "Tio João"),
+            new Produto("Feijão", 10.00f, 2.0f, "Camil"),
+            new Produto("Macarrão", 5.00f, 1.0f, "Barilla")
+        };
 
-    
-    }  
+        // Imprimindo produtos
+        Console.WriteLine("Produtos:");
+        foreach (var p in produtos)
+        {
+            p.ExibirProduto();
+        }
+
+        Console.WriteLine();
+
+        // Imprimindo clientes
+        Console.WriteLine("Clientes:");
+        foreach (var c in clientes)
+        {
+            Console.WriteLine(c);
+        }
+
+        Console.WriteLine();
+
+        // Acessando elementos específicos
+        Console.WriteLine("Cliente 3: " + clientes[2]);
+        Console.WriteLine("Produto 1: " + produtos[2].nome);
+    }
 }
