3943a71d19b0d1c7c359b152f50f91e6828af153
Claudinei Santos Goncalves <nei.sg@hotmail.com>
2025-04-24 17:11:49 -0300

    criando formatos de leitura

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 
 struct Produto
 {
@@ -26,6 +30,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        StreamWriter escritor = File.AppendText("teste.txt");
+        escritor.WriteLine("Victor");
+        escritor.WriteLine("Lima");
+        escritor.WriteLine("Curso de C#");
+        escritor.Close();
+        Console.WriteLine("Arquivo criado com sucesso!");
+        Console.ReadLine();
+
+
         // Lista de clientes
         List<string> clientes = new List<string> { "João", "Maria", "José", "Ana", "Pedro" };
 
